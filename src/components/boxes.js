af73e4aa048c37b2fa313b2482f2882b04c3c6e9
Sam Daived <37539198+samdaived@users.noreply.github.com>
2019-09-15 20:23:03 +0300

    Update boxes.js

--- src/components/boxes.js
+++ src/components/boxes.js
@@ -10,8 +10,8 @@ static contextType = TryContext;
 render(){
 
     return <div style={{display:'flex',marginLeft:'40vw'}}>
-        <Box  name="Sam" age="23"/>
-        <Box name="Laura" age="24"/>
+        <Box  name="Someone" age="23"/>
+        <Box name="Someone Else" age="24"/>
         
        <button onClick={this.context.click}>Hide/Show</button>
        
